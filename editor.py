9f11d90410a51e94aefd15fbb6f5d9dbf3b94d45
Marcin Fudala <nerko937@mail.com>
2018-10-11 08:39:43 +0200

    correction

--- editor.py
+++ editor.py
@@ -1,56 +1,46 @@
 from PIL import Image
 import os
 
-class ImageEditor():
 
-    def set_config(self, a):
-        self.IMAGE_W = a.IMAGE_W
-        self.IMAGE_H = a.IMAGE_H
-        self.IMAGE_C = a.IMAGE_C
-        self.ROTATE = a.ROTATE
-        self.ROTATE_REVERSE = a.ROTATE_REVERSE
-        self.WHITE_BLACK = a.WHITE_BLACK
+class ImageEditor:
+
+    def set_config(self, config_atts):
+        self.config = config_atts
 
     def __str__(self):
         return f'''
-       IMAGE_W = {self.IMAGE_W}
-       IMAGE_H = {self.IMAGE_H}
-       IMAGE_C = {self.IMAGE_C}
-       ROTATE = {self.ROTATE}
-       ROTATE_REVERSE = {self.ROTATE_REVERSE}
-       WHITE_BLACK = {self.WHITE_BLACK}'''
+       IMAGE_W = {self.config.IMAGE_W}
+       IMAGE_H = {self.config.IMAGE_H}
+       IMAGE_C = {self.config.IMAGE_C}
+       ROTATE = {self.config.ROTATE}
+       ROTATE_REVERSE = {self.config.ROTATE_REVERSE}
+       WHITE_BLACK = {self.config.WHITE_BLACK}'''
 
-    def cut(self):
-        print('Cutting images')
-        for x in os.listdir('media/'):
-            i = Image.open(f'media/{x}')
-            if self.IMAGE_W in range(1, i.size[0]) or self.IMAGE_H in range(1, i.size[1]):
-                if self.IMAGE_C == True:
-                    cntr1 = i.size[0] / 2
-                    cntr2 = i.size[1] / 2
-                    box = (cntr1, cntr2, (cntr1 + self.IMAGE_W), (cntr2 + self.IMAGE_H))
-                else:
-                    box = (0, 0, self.IMAGE_W, self.IMAGE_H)
-                i.crop(box).save(f'media/{x}')
+    def cut(self, i):
+        if self.config.IMAGE_W in range(1, i.size[0]) or self.config.IMAGE_H in range(1, i.size[1]):
+            if self.config.IMAGE_C is True:
+                cntr1 = i.size[0] / 2
+                cntr2 = i.size[1] / 2
+                return i.crop((cntr1, cntr2, (cntr1 + self.config.IMAGE_W), (cntr2 + self.config.IMAGE_H)))
+            else:
+                return i.crop((0, 0, self.config.IMAGE_W, self.config.IMAGE_H))
 
-    def roll(self):
-        if self.ROTATE in range(1, 360):
-            print('Rotating images')
-            for x in os.listdir('media/'):
-                i = Image.open(f'media/{x}')
-                if self.ROTATE_REVERSE == True:
-                    i.rotate(360 - self.ROTATE).save(f'media/{x}')
-                else:
-                    i.rotate(self.ROTATE).save(f'media/{x}')
+    def roll(self, i):
+        if self.config.ROTATE in range(1, 360):
+            if self.config.ROTATE_REVERSE is True:
+                return i.rotate(360 - self.config.ROTATE)
+            else:
+                return i.rotate(self.config.ROTATE)
 
-    def black_n_white(self):
-        if self.WHITE_BLACK == True:
-            print('Making images black and white')
-            for x in os.listdir('media/'):
-                i = Image.open(f'media/{x}')
-                i.convert(mode='L').save(f'media/{x}')
+    def black_n_white(self, i):
+        if self.config.WHITE_BLACK is True:
+            return i.convert(mode='L')
 
     def run(self):
-        self.cut()
-        self.roll()
-        self.black_n_white()
+        for x in os.listdir('media/'):
+            print(f'Editing {x}')
+            i = Image.open(f'media/{x}')
+            i = self.cut(i)
+            i = self.roll(i)
+            i = self.black_n_white(i)
+            i.save(f'media/{x}')
